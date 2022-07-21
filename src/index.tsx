091f1609ba8ea9089f3c26af5b6284e9d358fa8b
Ariane Brandao <ariane@ghbranding.com.br>
2022-07-21 20:48:41 -0300

    fix: add .env.example e formatação de código

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createServer, Model} from 'miragejs';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
 
-import { App } from './App';
+import { App } from "./App";
 
 createServer({
   //models = tables db
   models: {
     transaction: Model,
   },
-  seeds(server){
+  seeds(server) {
     server.db.loadData({
       transactions: [
         {
@@ -17,24 +17,24 @@ createServer({
           amount: 6000,
           type: "deposit",
           category: "Dev",
-          createdAt: new Date('2021-07-12 09:00:00'),
+          createdAt: new Date("2021-07-12 09:00:00"),
         },
         {
           title: "Aluguel",
           amount: 1000,
           type: "withdraw",
           category: "Casa",
-          createdAt: new Date('2021-07-14 14:00:00'),
+          createdAt: new Date("2021-07-14 14:00:00"),
         },
       ],
-    })
+    });
   },
-  routes(){
-    this.namespace = 'api';
+  routes() {
+    this.namespace = "api";
 
     //rota listagem
-    this.get('/transactions', ()=>{
-/*       return [
+    this.get("/transactions", () => {
+      /*       return [
         {
           id: 1,
           title: 'Transaction 1',
@@ -45,26 +45,25 @@ createServer({
         }
       ] */
 
-      return this.schema.all('transaction');
+      return this.schema.all("transaction");
     });
 
     //rota insert
-    this.post('/transactions', (schema, request)=>{
+    this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody); //converte texto em objeto
       const newData = {
         ...data,
         createdAt: new Date(),
-      }
+      };
 
-      return schema.create('transaction', newData); //schema = db, transaction = table
-    })
-
-  }
-})
+      return schema.create("transaction", newData); //schema = db, transaction = table
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
